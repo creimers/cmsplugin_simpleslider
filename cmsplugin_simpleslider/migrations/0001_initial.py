991333a73301d7526f957a92044c644ef5e27a1a
creimers <christoph@superservice-international.com>
2015-06-25 00:33:08 +0200

    updated migrations

--- cmsplugin_simpleslider/migrations/0001_initial.py
+++ cmsplugin_simpleslider/migrations/0001_initial.py
@@ -9,48 +9,48 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
+        ('filer', '0002_auto_20150606_2003'),
+        ('cms', '0003_auto_20140926_2347'),
         ('cmsplugin_filer_image', '0001_initial'),
-        ('cms', '0011_auto_20150419_1006'),
-        ('filer', '0001_initial'),
     ]
 
     operations = [
         migrations.CreateModel(
             name='Image',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
-                ('order', models.PositiveIntegerField(db_index=True, default=1, editable=False)),
-                ('caption_text', models.CharField(max_length=255, verbose_name='caption text', blank=True, null=True)),
-                ('image', filer.fields.image.FilerImageField(to='filer.Image', related_name='image')),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('order', models.PositiveIntegerField(default=1, editable=False, db_index=True)),
+                ('caption_text', models.CharField(max_length=255, null=True, verbose_name='caption text', blank=True)),
+                ('image', filer.fields.image.FilerImageField(related_name='Bild', to='filer.Image')),
             ],
             options={
-                'verbose_name_plural': 'images',
-                'abstract': False,
                 'ordering': ['order'],
+                'abstract': False,
+                'verbose_name_plural': 'images',
             },
             bases=(models.Model,),
         ),
         migrations.CreateModel(
             name='Slider',
             fields=[
-                ('cmsplugin_ptr', models.OneToOneField(primary_key=True, serialize=False, to='cms.CMSPlugin', parent_link=True, auto_created=True)),
-                ('order', models.PositiveIntegerField(db_index=True, default=1, editable=False)),
-                ('name', models.CharField(max_length=50, verbose_name='name', blank=True, null=True)),
-                ('dots', models.BooleanField(verbose_name='dots', default=False)),
-                ('fade', models.BooleanField(verbose_name='fade', default=False)),
-                ('autoplay', models.BooleanField(verbose_name='autoplay', default=True)),
-                ('image_options', models.ForeignKey(null=True, to='cmsplugin_filer_image.ThumbnailOption', on_delete=django.db.models.deletion.SET_NULL, related_name='djangocms_blog_post_thumbnail', verbose_name='image size', blank=True)),
+                ('cmsplugin_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='cms.CMSPlugin')),
+                ('order', models.PositiveIntegerField(default=1, editable=False, db_index=True)),
+                ('name', models.CharField(max_length=50, null=True, verbose_name='name', blank=True)),
+                ('dots', models.BooleanField(default=False, verbose_name='dots')),
+                ('fade', models.BooleanField(default=False, verbose_name='fade')),
+                ('autoplay', models.BooleanField(default=True, verbose_name='autoplay')),
+                ('image_options', models.ForeignKey(related_name='djangocms_blog_post_thumbnail', on_delete=django.db.models.deletion.SET_NULL, verbose_name='image size', blank=True, to='cmsplugin_filer_image.ThumbnailOption', null=True)),
             ],
             options={
-                'abstract': False,
                 'ordering': ['order'],
+                'abstract': False,
             },
             bases=('cms.cmsplugin', models.Model),
         ),
         migrations.AddField(
             model_name='image',
             name='slider',
-            field=models.ForeignKey(to='cmsplugin_simpleslider.Slider', related_name='images'),
+            field=models.ForeignKey(related_name='images', to='cmsplugin_simpleslider.Slider'),
             preserve_default=True,
         ),
     ]
